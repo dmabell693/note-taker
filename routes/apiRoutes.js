0becc31f75df67519d0e14fdb7bd8df1780a2dd4
Daniel Abell <da450@cairn.edu>
2020-04-25 21:31:47 -0400

    added 'routes' directory to house api and html routes and clean up 'server.js'. created post method for adding new notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const fs = require("fs");
+
+module.exports = function (app) {
+
+    app.get("/api/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../db/db.json"));
+    });
+
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+
+        newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
+        console.log(newNote.id);
+
+        fs.readFile(path.join(__dirname, "../db/db.json"), function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            let notes = JSON.parse(data);
+
+            console.log(notes);
+
+            notes.push(newNote);
+
+            console.log(notes);
+
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                res.json(notes);
+            });
+
+        });
+
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+        let chosen = req.params.id;
+        console.log(chosen);
+
+        
+    })
+
+}
